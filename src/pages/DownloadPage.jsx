3888dc398429e54fee42055cd9657487d4f81184
Alexander Duque <alexander.duque@hyd.com.es>
2023-07-13 10:45:52 +0200

    fix style buttons in downloadpage

--- src/pages/DownloadPage.jsx
+++ src/pages/DownloadPage.jsx
@@ -10,9 +10,9 @@ export const DownloadPage = () => {
       <div className='container'>
         <h2 className='mt-3'>On-board File Download</h2>
         <div className='container mt-4'>
-          <div className='col-5'>
-            <div className='row'>
-              <div>
+          <div className='row'>
+            <div className='col-6'>
+              <div className='row'>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
                   <span style={{ marginRight: '10px' }}>Type:</span>
                   <select className="form-select" aria-label="Default select example">
@@ -25,28 +25,25 @@ export const DownloadPage = () => {
                   </select>
                 </div>
               </div>
-            </div>
-            <div className='row'>
-              <div className='mt-4'>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
+              <div className='row'>
+                <div className='mt-2' style={{ display: 'flex', alignItems: 'center' }}>
                   <span style={{ marginRight: '10px' }}>Index:</span>
-                  <div className="input-group mb-3">
+                  <div className="input-group">
                     <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className='col-5'>
-            <div className='row mt-4'>
-              <Link className='btn btn-primary' to={`/page2`}>
-                Download
-              </Link>
-              <Link className='btn btn-secondary' to={`/page2`}>
-                Abort
-              </Link>
+            <div className='col-5 ps-4'>
+              <div className='row'>
+                <Link className='btn btn-primary' to={`/`}>
+                  Download
+                </Link>
+                <Link className='btn btn-secondary mt-2' to={`/`}>
+                  Abort
+                </Link>
+              </div>
             </div>
-
           </div>
         </div>
       </div>
